49c91ae0542f60cbd5413879d0baf8016a988b81
Quincy Garcia <quincygarcia1@gmail.com>
2021-09-13 21:03:42 -0400

    added remove and go-to buttons

--- popup.js
+++ popup.js
@@ -8,18 +8,23 @@ chrome.storage.sync.get(null, function(items){
             } else {
                 titleValue = items[key][0] + " "
             }
-            createSection(cutLink, titleValue, items[key][1])
+            createSection(cutLink, titleValue, items[key][1], key)
         } else {
             //pass
         }
     }
 })
 
-function createSection(link, Title, Snippet) {
+function createSection(link, Title, Snippet, keyValue) {
     var new_section = document.createElement("li")
     var titleScript = document.createElement("b")
     var codeHolder = document.createElement("pre")
     var codeSnippet = document.createElement("code")
+    var buttonDiv = document.createElement("div")
+    buttonDiv.className = "buttonSection"
+    var buttonDiv = document.createElement("div")
+    ButtonCreator("controlbuttons removeBtn", "Remove", buttonDiv, link)
+    ButtonCreator("controlbuttons goToBtn", "Go To Source", buttonDiv, link)
     codeHolder.className = "holder"
     codeHolder.appendChild(codeSnippet)
     new_section.className = 'snippetBox'
@@ -27,5 +32,16 @@ function createSection(link, Title, Snippet) {
     codeSnippet.innerHTML = Snippet
     new_section.appendChild(titleScript)
     new_section.appendChild(codeHolder)
+    new_section.appendChild(buttonDiv)
     document.getElementsByClassName("listSnippets")[0].appendChild(new_section)
+}
+
+function ButtonCreator(classTitle2, inner, appendsection, openLink){
+    var newBtn = document.createElement("button")
+    newBtn.className = classTitle2
+    newBtn.innerHTML = inner
+    if(classTitle2 === "controlbuttons goToBtn"){
+        newBtn.addEventListener("click", function(){window.open(openLink, '_blank')})
+    }
+    appendsection.appendChild(newBtn)
 }

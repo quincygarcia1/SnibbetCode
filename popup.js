92d8ec6f0e54f797e8de9c5058b7bebf2d83d033
Quincy Garcia <quincygarcia1@gmail.com>
2021-09-24 23:33:57 -0400

    Added comments and fixed variable names for readability

--- popup.js
+++ popup.js
@@ -1,7 +1,9 @@
-function setup(){
+function Setup(){
+    // Search Chrome storage for all link-snippet pairs that should be displayed on the popup
     chrome.storage.sync.get(null, function(items){
         for (key in items) {
-            if (key.includes("Snibbet - ")){
+            // "Snibbit - " is the identifier for elements of the Chrome extension
+            if (key.includes("Snibbit - ")){
                 var cutLink = key.slice(10);
                 var titleValue;
                 if (items[key][0] === ""){
@@ -9,18 +11,18 @@ function setup(){
                 } else {
                     titleValue = items[key][0] + " "
                 }
-                createSection(cutLink, titleValue, items[key][1], key)
+                CreateSection(cutLink, titleValue, items[key][1], key)
             } else {
                 //pass
             }
         }
     })
 }
-setup();
+Setup();
 
-function createSection(link, Title, Snippet, keyValue) {
-    var new_section = document.createElement("li")
-    new_section.id = keyValue
+function CreateSection(link, Title, Snippet, keyValue) {
+    var newSection = document.createElement("li")
+    newSection.id = keyValue
     var titleScript = document.createElement("b")
     var codeHolder = document.createElement("pre")
     var codeSnippet = document.createElement("code")
@@ -31,16 +33,17 @@ function createSection(link, Title, Snippet, keyValue) {
     ButtonCreator("controlbuttons goToBtn", "Go To Source", buttonDiv, link, keyValue)
     codeHolder.className = "holder"
     codeHolder.appendChild(codeSnippet)
-    new_section.className = 'snippetBox'
+    newSection.className = 'snippetBox'
     titleScript.innerHTML = Title
     codeSnippet.innerHTML = Snippet
-    new_section.appendChild(titleScript)
-    new_section.appendChild(codeHolder)
-    new_section.appendChild(buttonDiv)
-    document.getElementsByClassName("listSnippets")[0].appendChild(new_section)
+    newSection.appendChild(titleScript)
+    newSection.appendChild(codeHolder)
+    newSection.appendChild(buttonDiv)
+    document.getElementsByClassName("listSnippets")[0].appendChild(newSection)
 }
 
-function ButtonCreator(classTitle2, inner, appendsection, openLink, keyValue){
+function ButtonCreator(classTitle2, inner, appendsection, openLink, keyValue) {
+    // Function that will build the inner "remove" and "open link" buttons within each snippet
     var newBtn = document.createElement("button")
     newBtn.className = classTitle2
     newBtn.innerHTML = inner
@@ -49,8 +52,8 @@ function ButtonCreator(classTitle2, inner, appendsection, openLink, keyValue){
     }else if(classTitle2 === "controlbuttons removeBtn"){
         newBtn.addEventListener("click", function(){
             chrome.storage.sync.remove(keyValue)
-            l = document.getElementsByTagName("ul")[0]
-            l.removeChild(document.getElementById(keyValue))
+            listIndex = document.getElementsByTagName("ul")[0]
+            listIndex.removeChild(document.getElementById(keyValue))
         })
     }
     appendsection.appendChild(newBtn)
